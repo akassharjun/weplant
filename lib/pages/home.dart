e691790a8c9a8b36d3281c47d4ef8a955d335349
Akassharjun Shanmugarajah <akassharjun@gmail.com>
2019-08-21 19:08:20 +0530

    Fixed Bitmap Description icon size issue in iOS.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_screen_scaler/flutter_screen_scaler.dart';
@@ -10,8 +11,6 @@ import 'package:we_plant/pages/plant_information.dart';
 import 'package:we_plant/pages/search_by_country.dart';
 import 'package:we_plant/scoped_model/home.dart';
 
-ScreenScaler scaler;
-
 class HomePage extends StatefulWidget {
   static const String ROUTE = "home";
 
@@ -22,23 +21,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   Completer<GoogleMapController> _controller = Completer();
   final HomeModel homeModel = HomeModel();
+  ScreenScaler scaler;
 
   static LatLng _center = LatLng(6.865183, 79.859787);
-
   var location = new Location();
-
   Set<Marker> _markers = new Set();
-
   LatLng _lastMapPosition = _center;
 
   bool foundLocation = false;
-  bool isFirstTime = true;
 
   @override
   void initState() {
     super.initState();
     _addTreeMarkers();
-    isFirstTime = false;
   }
 
   @override
@@ -51,10 +46,11 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     }
   }
 
+  // Methods
+  
   Future _addTreeMarkers() async {
     await homeModel.retrievePreviousPlantInformation();
-		print("Debugging");
-		print(homeModel.plantInformationList);
+
     homeModel.plantInformationList.forEach((plantInformation) {
       _addMarker(plantInformation);
     });
@@ -63,8 +59,13 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   void _addMarker(PlantInformation plantInformation) async {
     ImageConfiguration imageConfiguration =
         new ImageConfiguration(size: Size(5, 5));
-    BitmapDescriptor bitmapDescriptor = await BitmapDescriptor.fromAssetImage(
-        imageConfiguration, 'assets/tree.png');
+    String assetName = 'assets/tree';
+
+    Platform.isIOS ? assetName += "_ios.png" : assetName += "_android.png";
+
+    print(assetName);
+    BitmapDescriptor bitmapDescriptor =
+        await BitmapDescriptor.fromAssetImage(imageConfiguration, assetName);
     setState(() {
       _markers.add(Marker(
         // This marker id can be anything that uniquely identifies each marker.
@@ -116,6 +117,126 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     });
   }
 
+  // Widgets
+  
+  GoogleMap _buildGoogleMap() {
+    return GoogleMap(
+      padding: EdgeInsets.all(5.0),
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 10.0,
+      ),
+      mapType: MapType.normal,
+      markers: _markers,
+      onCameraMove: _onCameraMove,
+    );
+  }
+
+  Widget _buildBottomToolBar(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      right: 0,
+      child: Padding(
+        padding: EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: _goToTheCurrentLocation,
+              child: Icon(
+                Icons.my_location,
+                color: Colors.black54,
+                ),
+              backgroundColor: Colors.white,
+              ),
+            SizedBox(height: scaler.getHeight(2)),
+            FloatingActionButton.extended(
+              heroTag: "Fab",
+              onPressed: () {
+                Navigator.pushNamed(context, PlantInformationScreen.ROUTE);
+              },
+              label: Text(
+                'Plant A Tree',
+                style: TextStyle(
+                  fontSize: scaler.getTextSize(11),
+                  ),
+                ),
+              icon: Icon(Icons.toys),
+              ),
+          ],
+          ),
+        ),
+      );
+  }
+
+  Widget _buildTopToolbar(BuildContext context) {
+    return Positioned(
+      top: 5,
+      right: 5,
+      child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(context,
+                                 MaterialPageRoute(builder: (BuildContext context) {
+                                   return SearchByCountryPage(homeModel.plantInformationList);
+                                 }));
+            },
+            heroTag: "Find",
+            child: Icon(
+              Icons.search,
+              color: Colors.black54,
+              ),
+            backgroundColor: Colors.white,
+            )),
+      );
+  }
+
+  Widget _buildAppLogo() {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white70,
+          borderRadius: BorderRadius.circular(20.0),
+          ),
+        height: scaler.getHeight(18),
+        width: scaler.getHeight(18),
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    height: scaler.getHeight(10),
+//                          color: Colors.black,
+                    child: Image.asset('assets/plant.png'),
+                    ),
+                  ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "WePlant",
+//                        textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: scaler.getTextSize(15),
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF252525),
+                      ),
+                    ),
+                  ),
+              ],
+              ),
+            ),
+          ),
+        ),
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     scaler = new ScreenScaler()..init(context);
@@ -126,118 +247,16 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
         body: SafeArea(
           child: Stack(
             children: <Widget>[
-              GoogleMap(
-                padding: EdgeInsets.all(5.0),
-                onMapCreated: _onMapCreated,
-                initialCameraPosition: CameraPosition(
-                  target: _center,
-                  zoom: 10.0,
-                ),
-                mapType: MapType.normal,
-                markers: _markers,
-                onCameraMove: _onCameraMove,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white70,
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  height: scaler.getHeight(18),
-                  width: scaler.getHeight(18),
-                  child: Center(
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Align(
-                            alignment: Alignment.center,
-                            child: Container(
-                              height: scaler.getHeight(10),
-//                          color: Colors.black,
-                              child: Image.asset('assets/plant.png'),
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              "WePlant",
-//                        textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: scaler.getTextSize(15),
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFF252525),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 5,
-                right: 5,
-                child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FloatingActionButton(
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (BuildContext context) {
-                          return SearchByCountryPage(
-                              homeModel.plantInformationList);
-                        }));
-                      },
-                      heroTag: "Find",
-                      child: Icon(
-                        Icons.search,
-                        color: Colors.black54,
-                      ),
-                      backgroundColor: Colors.white,
-                    )),
-              ),
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: Padding(
-                  padding: EdgeInsets.all(12.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: <Widget>[
-                      FloatingActionButton(
-                        onPressed: _goToTheCurrentLocation,
-                        child: Icon(
-                          Icons.my_location,
-                          color: Colors.black54,
-                        ),
-                        backgroundColor: Colors.white,
-                      ),
-                      SizedBox(height: scaler.getHeight(2)),
-                      FloatingActionButton.extended(
-                        heroTag: "Fab",
-                        onPressed: () {
-                          Navigator.pushNamed(
-                              context, PlantInformationScreen.ROUTE);
-                        },
-                        label: Text(
-                          'Plant A Tree',
-                          style: TextStyle(
-                            fontSize: scaler.getTextSize(11),
-                          ),
-                        ),
-                        icon: Icon(Icons.toys),
-                      ),
-                    ],
-                  ),
-                ),
-              )
+              _buildGoogleMap(),
+              _buildAppLogo(),
+              _buildTopToolbar(context),
+              _buildBottomToolBar(context)
             ],
           ),
         ),
       ),
     );
   }
+
+ 
 }
